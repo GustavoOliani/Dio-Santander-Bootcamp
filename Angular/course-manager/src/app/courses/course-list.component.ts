eb4bdd0fe4be60242576e0be978d612f1e6ca648
Oliani <oliani748@gmail.com>
2022-07-07 11:17:56 -0300

    Creating project Course-List
    Added the courses file and it's components

---
+++ Angular/course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: "app-course-list",
+    templateUrl: "./course-list.component.html"
+})
+export class CourseListComponent implements OnInit{
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: "Angular: Forms",
+                imageUrl: "",
+                price: 0.01,
+                code: "A-1",
+                duration: 120,
+                rating: 5.0,
+                releaseDate: "July, 6, 2022"
+            },
+            {
+                id: 2,
+                name: "Angular: HTTP",
+                imageUrl: "",
+                price: 0.02,
+                code: "A-2",
+                duration: 80,
+                rating: 4.0,
+                releaseDate: "July, 7, 2022"
+            }
+    
+        ];
+    }
+
+}
